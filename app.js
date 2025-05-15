e389447525ca7f9513efc4032d649106a577818b
DanielAlejandroDuran <166954580+DanielAlejandroDuran@users.noreply.github.com.>
2025-05-15 15:50:31 -0500

    feat: Creacion y Definicion de Variales Globales

---
+++ app.js
@@ -0,0 +1,15 @@
+let personajesDC = []
+let personajesMarvel = []
+let modoJuego = ""
+let universoSeleccionado = ""
+let personajeSeleccionado1 = null
+let personajeSeleccionado2 = null
+let turnoJugador1 = true
+let vidaInicial1 = 100
+let vidaInicial2 = 100
+const estadisticasCombate = {
+    dañoRealizado: 0,
+    dañoRecibido: 0,
+    ataquesCriticos: 0,
+    ataquesDebiles: 0,
+}
